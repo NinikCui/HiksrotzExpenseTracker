bd427c9346e6cff5718337f4ea7595eb5577237b
nferrarel@gmail.com <nferrarel@gmail.com>
2025-05-22 15:31:44 +0700

    File Set Up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.hiksrot.hiksrotzexpensetracker"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.hiksrot.hiksrotzexpensetracker"
         minSdk = 24
