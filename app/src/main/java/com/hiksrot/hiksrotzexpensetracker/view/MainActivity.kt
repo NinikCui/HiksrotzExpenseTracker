635112f2e840b859f6d2a303f3bad7637128faaa
arvin <nferrarel@gmail.com>
2025-05-26 17:58:26 +0700

    Modul 1 Prototype  UI

--- app/src/main/java/com/hiksrot/hiksrotzexpensetracker/view/MainActivity.kt
+++ app/src/main/java/com/hiksrot/hiksrotzexpensetracker/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.hiksrot.hiksrotzexpensetracker
+package com.hiksrot.hiksrotzexpensetracker.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hiksrot.hiksrotzexpensetracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
