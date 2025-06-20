8a0ea031a1f4e3cd734b22655f178508ce97e1f8
Nico <160150414+NinikCui@users.noreply.github.com>
2025-06-20 17:29:16 +0700

    Bottom Navbar

--- app/src/main/java/com/hiksrot/hiksrotzexpensetracker/view/MainActivity.kt
+++ app/src/main/java/com/hiksrot/hiksrotzexpensetracker/view/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.hiksrot.hiksrotzexpensetracker.R
 import com.hiksrot.hiksrotzexpensetracker.databinding.ActivityLogregBinding
 import com.hiksrot.hiksrotzexpensetracker.databinding.ActivityMainBinding
@@ -13,15 +17,15 @@ import com.hiksrot.hiksrotzexpensetracker.viewmodel.MainViewModel
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
-
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val username = intent.getStringExtra("username")
 
-        binding.tvUsername.text = "Selamat datang, $username!"
-
+        navController = (supportFragmentManager.findFragmentById(R.id.fragmentHost) as NavHostFragment).navController
+        binding.bottomNav.setupWithNavController(navController)
 
     }
 }
