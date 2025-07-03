16bf204681d3315ce71ae3c73d90dd4c7248462f
ariril <arilmanek88@gmail.com>
2025-07-03 20:28:41 +0700

    Implement Light Sensor

--- app/src/main/java/com/hiksrot/hiksrotzexpensetracker/util/AppDatabaseCallback.kt
+++ app/src/main/java/com/hiksrot/hiksrotzexpensetracker/util/AppDatabaseCallback.kt
@@ -20,9 +20,9 @@ class AppDatabaseCallback : RoomDatabase.Callback() {
         db.execSQL("INSERT INTO budgets (id, name, amount, user_id, created_at, month, year) VALUES (5, 'Hiburan', 400000, 3, 1718300004, 7, 2025)")
 
         // ==== Insert Expense Dummy ====
-        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (1, 'Nasi Goreng enak pedis pedas uyeaahhhhhhhhhhhhhhhhhhhh', 25000, 1749988800000, 1, 1)")
-        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (6, 'Beli Es Teh Jumbo', 50000, 1749996000000, 2, 1)")
-        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (2, 'Naik Gojek ngiiiiihhaaaaaaa', 18000, 1749992400000, 2, 1)")
+        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (1, 'Nasi Goreng enak pedis pedas uyeaahhhhhhhhhhhhhhhhhhhh', 25000, 1751548112000, 1, 1)")
+        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (6, 'Beli Es Teh Jumbo', 50000, 1751461712000, 2, 1)")
+        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (2, 'Naik Gojek ngiiiiihhaaaaaaa', 18000, 1751375312000, 2, 1)")
 
         db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (3, 'Ayam Geprek', 28000, 1749772800000, 3, 2)")
         db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (4, 'Boba Drink', 22000, 1749772800000, 4, 2)")
@@ -33,17 +33,17 @@ class AppDatabaseCallback : RoomDatabase.Callback() {
         db.execSQL("INSERT INTO budgets (id, name, amount, user_id, created_at, month, year) VALUES (6, 'Internet', 250000, 1, 1718300100, 6, 2025)")
         db.execSQL("INSERT INTO budgets (id, name, amount, user_id, created_at, month, year) VALUES (7, 'Kopi', 150000, 1, 1718300200, 6, 2025)")
 
-        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (7, 'Bayar Wifi Kost', 120000, 1719936000000, 6, 1)")
-        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (8, 'Beli Paket Data', 30000, 1720022400000, 6, 1)")
+        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (7, 'Bayar Wifi Kost', 120000, 1749042512000, 6, 1)")
+        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (8, 'Beli Paket Data', 30000, 1748956112000, 6, 1)")
 
-        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (9, 'Ngopi di Starbucks', 60000, 1719936000000, 7, 1)")
-        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (10, 'Beli Kopi Literan', 50000, 1720022400000, 7, 1)")
+        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (9, 'Ngopi di Starbucks', 60000, 1748869712000, 7, 1)")
+        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (10, 'Beli Kopi Literan', 50000, 1748783312000, 7, 1)")
 
         // data tambahan bulan 7
         db.execSQL("INSERT INTO budgets (id, name, amount, user_id, created_at, month, year) VALUES (8, 'Buku', 100000, 1, 1718300300, 5, 2025)")
 
-        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (11, 'Beli Novel', 60000, 1717267200000, 8, 1)")
-        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (12, 'Beli Buku Kuliah', 30000, 1717353600000, 8, 1)")
+        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (11, 'Beli Novel', 60000, 1746191312000, 8, 1)")
+        db.execSQL("INSERT INTO expenses (id, description, amount, date, budget_id, user_id) VALUES (12, 'Beli Buku Kuliah', 30000, 1746277712000, 8, 1)")
 
 
     }
--- app/src/main/java/com/hiksrot/hiksrotzexpensetracker/view/MainActivity.kt
+++ app/src/main/java/com/hiksrot/hiksrotzexpensetracker/view/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.hiksrot.hiksrotzexpensetracker.view
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -9,15 +14,22 @@ import com.hiksrot.hiksrotzexpensetracker.R
 import com.hiksrot.hiksrotzexpensetracker.databinding.ActivityMainBinding
 import com.hiksrot.hiksrotzexpensetracker.viewmodel.LoginRegisterViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: LoginRegisterViewModel
+
+    private lateinit var sensorManager: SensorManager
+    private var lightSensor: Sensor? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
         viewModel = ViewModelProvider(this)[LoginRegisterViewModel::class.java]
 
@@ -59,5 +71,34 @@ class MainActivity : AppCompatActivity() {
                     else -> false
                 }
             }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lightSensor?.also { light ->
+            sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        event?.let {
+            if (it.sensor.type == Sensor.TYPE_LIGHT) {
+                val lightLevel = it.values[0]
+                if (lightLevel < 50) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+
     }
 }
